9f7af376768e6432c42aa4cf888ef80720773bbe
killer-cf <costa.kilder@live.com>
2023-03-22 17:31:42 -0300

    config: styled components and fonts

---
+++ src/themes/deafult.ts
@@ -0,0 +1,35 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+
+    gray100: '#1B1D1E',
+    gray200: '#333638',
+    gray300: '#5C6265',
+    gray400: '#B9BBBC',
+    gray500: '#DDDEDF',
+    gray600: '#DDDEDF',
+    gray700: '#FAFAFA',
+
+    green_light: '#639339',
+    green_mid: '#CBE4B4',
+    green_dark: '#E5F0DB',
+
+    red_light: '#F4E6E7',
+    red_mid: '#F3BABD',
+    red_dark: '#BF3B44',
+
+  },
+  fontSize: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
+    xxl: 32
+  },
+  fontFamily: {
+    regular: 'NunitoSans_400Regular',
+    bold: 'NunitoSans_700Bold'
+  }
+}
